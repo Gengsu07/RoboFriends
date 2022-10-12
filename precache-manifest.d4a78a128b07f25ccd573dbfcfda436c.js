8655ca74ec73080e37d9c3c13bc18d4124c3d25e
Sugeng Wahyudi <sugengdatascience@gmail.com>
2022-10-12 10:59:29 +0700

    Updates

---
+++ precache-manifest.d4a78a128b07f25ccd573dbfcfda436c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "327758cb4c56ca6c6717",
+    "url": "/RoboFriends/static/css/main.249fac48.chunk.css"
+  },
+  {
+    "revision": "327758cb4c56ca6c6717",
+    "url": "/RoboFriends/static/js/main.4026f295.chunk.js"
+  },
+  {
+    "revision": "c01ddd688eb4a528c043",
+    "url": "/RoboFriends/static/js/runtime~main.84a40e82.js"
+  },
+  {
+    "revision": "252625372a540ad28e8f",
+    "url": "/RoboFriends/static/css/2.739768fb.chunk.css"
+  },
+  {
+    "revision": "252625372a540ad28e8f",
+    "url": "/RoboFriends/static/js/2.a57b84b1.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/RoboFriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "b1438d84e10080906a96f5bc33c90f8d",
+    "url": "/RoboFriends/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.018634b954d4109b372d4a33c0598584.js"
+  "/RoboFriends/precache-manifest.d4a78a128b07f25ccd573dbfcfda436c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/robofriends/index.html", {
+workbox.routing.registerNavigationRoute("/RoboFriends/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
