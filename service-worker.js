32308ad6f122e20fb28578b09df6aa713099c2cb
Sugeng Wahyudi <sugengdatascience@gmail.com>
2022-10-12 10:52:33 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Gengsu07/RoboFriends/precache-manifest.8f483d67c8a4d675b0ddd6e4355f695c.js"
+  "/robofriends/precache-manifest.018634b954d4109b372d4a33c0598584.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Gengsu07/RoboFriends/index.html", {
+workbox.routing.registerNavigationRoute("/robofriends/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
