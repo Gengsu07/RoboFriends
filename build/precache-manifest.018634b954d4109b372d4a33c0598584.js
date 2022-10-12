dd4fb232763b025eeaca9b5326cd3c18488f6c1b
Sugeng Wahyudi <sugengdatascience@gmail.com>
2022-10-12 10:58:21 +0700

    ok

---
+++ build/precache-manifest.018634b954d4109b372d4a33c0598584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "327758cb4c56ca6c6717",
+    "url": "/robofriends/static/css/main.249fac48.chunk.css"
+  },
+  {
+    "revision": "327758cb4c56ca6c6717",
+    "url": "/robofriends/static/js/main.4026f295.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "252625372a540ad28e8f",
+    "url": "/robofriends/static/css/2.739768fb.chunk.css"
+  },
+  {
+    "revision": "252625372a540ad28e8f",
+    "url": "/robofriends/static/js/2.a57b84b1.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "8248bb5755265fd0ee017cff034fe30f",
+    "url": "/robofriends/index.html"
+  }
+];
